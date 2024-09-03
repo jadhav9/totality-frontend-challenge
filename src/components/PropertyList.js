e1364c62cedf5d051a8b6fc3814b05331412daf1
unknown <sameerallmca@gmail.com>
2024-09-03 17:56:13 +0530

    Change

--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -4,7 +4,7 @@ import './PropertyList.css';
 
 const PropertyList = ({ filters = {} }) => {
   const properties = [
-    { id: 1, title: 'Cozy Apartment', price: 50000, location: 'Mumbai', bedrooms: 2, image: 'https://www.freepik.com/free-photo/spacious-room-with-big-window_968435.htm#fromView=search&page=1&position=1&uuid=55ab0997-8400-4481-a3cd-bd640cef7d77://via.placeholder.com/300x200?text=Cozy+Apartment' },
+    { id: 1, title: 'Cozy Apartment', price: 50000, location: 'Mumbai', bedrooms: 2, image: 'https://via.placeholder.com/300x200?text=Cozy+Apartment' },
     { id: 2, title: 'Luxury Villa', price: 100000, location: 'Delhi', bedrooms: 4, image: 'https://via.placeholder.com/300x200?text=Luxury+Villa' },
     { id: 3, title: 'High Floor Apartment', price: 35000, location: 'Pune', bedrooms: 2, image: 'https://via.placeholder.com/300x200?text=High+Floor+Apartment' },
     { id: 4, title: 'Luxury Villa', price: 150000, location: 'Delhi', bedrooms: 3, image: 'https://via.placeholder.com/300x200?text=Luxury+Villa' },
